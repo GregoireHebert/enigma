05e86e2b9dd4a0e21e58e2fcfd9a9a2a5e74bcc6
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-11-05 13:05:36 +0100

    add selection

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -29,6 +31,16 @@ class Order
      * @ORM\Column(type="integer")
      */
     private $amount;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Selection", mappedBy="myOrder", orphanRemoval=true)
+     */
+    private $selections;
+
+    public function __construct()
+    {
+        $this->selections = new ArrayCollection();
+    }
 
     /**
      * @return int
@@ -76,5 +88,36 @@ class Order
     public function setAmount(int $amount): void
     {
         $this->amount = $amount;
+    }
+
+    /**
+     * @return Collection|Selection[]
+     */
+    public function getSelections(): Collection
+    {
+        return $this->selections;
+    }
+
+    public function addSelection(Selection $selection): self
+    {
+        if (!$this->selections->contains($selection)) {
+            $this->selections[] = $selection;
+            $selection->setMyOrder($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSelection(Selection $selection): self
+    {
+        if ($this->selections->contains($selection)) {
+            $this->selections->removeElement($selection);
+            // set the owning side to null (unless already changed)
+            if ($selection->getMyOrder() === $this) {
+                $selection->setMyOrder(null);
+            }
+        }
+
+        return $this;
     }
 }
---
+++ src/Entity/Selection.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SelectionRepository")
+ */
+class Selection
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Order::class, inversedBy="selections")
+     * @ORM\JoinColumn(nullable=false, referencedColumnName="number")
+     */
+    private $myOrder;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Product")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $product;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMyOrder(): ?Order
+    {
+        return $this->myOrder;
+    }
+
+    public function setMyOrder(?Order $myOrder): self
+    {
+        $this->myOrder = $myOrder;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+}
