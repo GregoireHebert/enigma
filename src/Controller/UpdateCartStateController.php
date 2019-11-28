80f37b655592932c76ed6e2de8b7fca38bdc12b5
Shadowzyx <sh4dowzyx@gmail.com>
2019-11-28 14:38:29 +0100

    Security management (you can't update a cart's state if you aren't manager)

--- src/Controller/UpdateCartStateController.php
+++ src/Controller/UpdateCartStateController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Cart;
 use App\Services\CartStateUpdateService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route(path="/carts/{id}/update-state", name="update_state")
+ * @Security("is_granted('ROLE_MANAGER')")
  */
 class UpdateCartStateController extends AbstractController
 {
