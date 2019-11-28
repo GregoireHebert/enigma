29556e4e693252cbbd3694618c42e3032023abda
jleleu42 <jlu59@live.fr>
2019-11-28 14:21:16 +0100

    display command status

--- src/Controller/ListCartsController.php
+++ src/Controller/ListCartsController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Status;
 use App\Repository\CartRepository;
+use App\Repository\StatusRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -14,10 +16,15 @@ use Twig\Environment;
  */
 class ListCartsController
 {
-    public function __invoke(Environment $twig, CartRepository $cartRepository)
+
+    public function __invoke(Environment $twig, CartRepository $cartRepository, StatusRepository $statusRepository)
     {
+
+        //var_dump($statusRepository->findAll());
+
         return new Response($twig->render('cartList.html.twig', [
             'carts' => $cartRepository->findAll()
+            //'status' => $statusRepository->findId()
         ]));
     }
 }
--- src/Repository/StatusRepository.php
+++ src/Repository/StatusRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 /**
  * @method Status|null find($id, $lockMode = null, $lockVersion = null)
  * @method Status|null findOneBy(array $criteria, array $orderBy = null)
- * @method Status[]    findAll()
  * @method Status[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class StatusRepository extends ServiceEntityRepository
@@ -19,32 +18,8 @@ class StatusRepository extends ServiceEntityRepository
         parent::__construct($registry, Status::class);
     }
 
-    // /**
-    //  * @return Status[] Returns an array of Status objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findAll(): array
     {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+        return parent::findAll();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Status
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
