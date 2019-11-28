ae182004b0d02bce1593d00c336bf4d3c18824a2
MRY <yannick@redison.com>
2019-11-28 13:54:07 +0100

    did create the Entity for the cartStatus && cartStatus.yml to populate the table

---
+++ src/Entity/CartStatus.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class CartStatus
+{
+    /**
+     * @var int
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     */
+    private $id;
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $statusDescription;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatusDescription(): string
+    {
+        return $this->statusDescription;
+    }
+
+    /**
+     * @param string $statusDescription
+     */
+    public function setStatusDescription(string $statusDescription): void
+    {
+        $this->statusDescription = $statusDescription;
+    }
+
+}
