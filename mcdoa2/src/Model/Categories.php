6dfd87a17f0c8cb209659235d8a4af0b4765343f
j12l <julien120600@live.fr>
2019-12-03 10:27:30 +0100

    Création interface catégories

--- mcdoa2/src/Model/Categories.php
+++ mcdoa2/src/Model/Categories.php
@@ -7,6 +7,4 @@ interface Categories {
     public function setName(string $name): void;
     public function getId() :int;
     public function setId(int $id ) :void;
-    public function getProducts() : Products ;
-    public function setProducts(Products $products  ) :void;
 }
