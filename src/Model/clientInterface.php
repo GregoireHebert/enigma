53d3bab83bc4b787d1a6a2efe911f3b3a20de4f3
LeonNiard <58431692+LeonNiard@users.noreply.github.com>
2019-12-03 10:21:36 +0100

    Mcdoa2 (#14)
    * ajout interfaceClient
    
    * Ajout interfaces
    
    * Modification du fichier après vérification
    
    * Update

---
+++ src/Model/clientInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+interface clientInterface {
+
+    function setId(int $id): int;
+    function getId(): int;
+    function setNom(string $nom): string;
+    function getNom(): string;
+    function setAdresse(string $adresse): string;
+    function getAdresse(): string;
+
+    //Les points de fidélité ne sont pas définis au niveau des produits.
+    /*function setPointFidelite(int $point): int;
+    function getPointFidelite(): int;
+    function addPointFidelite(): int;
+    function removePointFidelite(): int;*/
+
+    function setOrders(array $orders);
+    function getOrders(): Array;
+    function addOrder(InterfaceCommande $order): Array;
+}
