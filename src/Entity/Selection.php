114c2ccb731938841fc00203d6e875f62f31ab50
Léon Niard <leon@MacBook-Pro-de-Leon.local>
2019-12-03 12:04:45 +0100

    Ajout entité

---
+++ src/Entity/Selection.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Model\interfaceSelection;
+use App\Model\Products;
+
+class Selection implements interfaceSelection{
+
+    private $produit;
+    private $quantite;
+
+
+    public function getProduit(): Products
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(Products $InterfaceProduct)
+    {
+        $this->produit = $InterfaceProduct;
+    }
+
+    public function getQuantite(): int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite)
+    {
+        $this->quantite = $quantite;
+    }
+
+    public function ajoutQuantite(int $quantite)
+    {
+        $this->quantite += $quantite;
+    }
+
+    public function reduireQuantite(int $quantite)
+    {
+        $this->quantite -= $quantite;
+    }
+}
